194a3a2a6ffd57a9ddac160abc5614693125dc8d
superPershing <727342027@qq.com>
2019-05-10 16:57:42 +0800

    feat(*): init entity

--- src/main/java/com/example/employee/entity/Company.java
+++ src/main/java/com/example/employee/entity/Company.java
@@ -1,5 +1,23 @@
 package com.example.employee.entity;
 
-public class Company {
 
+import lombok.Data;
+
+import javax.persistence.Entity;
+
+@Data
+@Entity
+public class Company {
+    private int id;
+    private String companyName;
+    private int employeesNumber;
+
+    public Company() {
+    }
+
+    public Company(int id, String companyName, int employeesNumber) {
+        this.id = id;
+        this.companyName = companyName;
+        this.employeesNumber = employeesNumber;
+    }
 }
--- src/main/java/com/example/employee/entity/Employee.java
+++ src/main/java/com/example/employee/entity/Employee.java
@@ -1,5 +1,30 @@
 package com.example.employee.entity;
 
+import lombok.Data;
+
+import javax.persistence.Entity;
+
+
+@Data
+@Entity
 public class Employee {
 
+    private int id;
+    private String name;
+    private int age;
+    private String gender;
+    private int companyId;
+    private int salary;
+
+    public Employee() {
+    }
+
+    public Employee(int id, String name, int age, String gender, int companyId, int salary) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+        this.gender = gender;
+        this.companyId = companyId;
+        this.salary = salary;
+    }
 }
